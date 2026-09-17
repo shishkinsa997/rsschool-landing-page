642cd4ec4e2459e2d8001411d90e74101b1503dc
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-09-17 11:27:09 +0500

    feat: setup vite project, set config

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import path from "path";
+import { resolve } from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+    sourcemap: true,
+    minify: false,
+    terserOptions: {
+      compress: false,
+      mangle: false,
+    },
+    rollupOptions: {
+      input: {
+        main: path.resolve(import.meta.dirname, "index.html"),
+        // catalog: path.resolve(import.meta.dirname, "catalog.html"),
+      },
+    },
+  },
+  server: {
+    host: "127.0.0.1",
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    host: "127.0.0.1",
+    port: 8080,
+    open: true,
+  },
+});
