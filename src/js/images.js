6e26bb63e18bc47382ed0831358133d03241f6cd
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-09-17 23:51:28 +0500

    feat: add helper for images

---
+++ src/js/images.js
@@ -0,0 +1,20 @@
+const images = import.meta.glob("../assets/img/*.{jpg,png,webp}", {
+  eager: true,
+});
+const icons = import.meta.glob("../assets/icons/*.svg", { eager: true });
+
+const imagePaths = Object.fromEntries(
+  Object.entries(images).map(([path, module]) => [
+    path.split("/").pop().split(".")[0],
+    module.default,
+  ]),
+);
+
+const iconPaths = Object.fromEntries(
+  Object.entries(icons).map(([path, module]) => [
+    path.split("/").pop().split(".")[0],
+    module.default,
+  ]),
+);
+
+export { imagePaths, iconPaths };
