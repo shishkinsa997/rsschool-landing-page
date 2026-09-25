d3cfff17fbad9a331cc7aa4e8718b8116fc15d33
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-09-25 21:18:19 +0500

    fix: change about section, change images to grid

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -10,22 +10,18 @@ export function renderAbout() {
     <div class="about-inner">
       <h2 class="h2">Resource is <span>the perfect and cozy place</span> where you can enjoy a variety of hot beverages, relax, catch up with friends, or get some work done.</h1>
       <div class='gallery'>
-        <div class="column">
-          <div class="image-box about1">
+          <div class="image-box">
             <img src="${imagePaths["about-1"]}" alt="">
           </div>
-          <div class="image-box about2">
+          <div class="image-box">
             <img src="${imagePaths["about-2"]}" alt="">
           </div>
-        </div>
-        <div class="column">
-          <div class="image-box about3">
+          <div class="image-box">
             <img src="${imagePaths["about-3"]}" alt="">
           </div>
-          <div class="image-box about4">
+          <div class="image-box">
             <img src="${imagePaths["about-4"]}" alt="">
           </div>
-        </div>
       </div>
     </div>
   `;
